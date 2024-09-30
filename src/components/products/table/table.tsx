74e7953df33665d471609ecd1ed737096afbc96c
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2024-09-30 20:12:00 +0300

    Set table view grid

--- src/components/products/table/table.tsx
+++ src/components/products/table/table.tsx
@@ -32,24 +32,25 @@ export const ProductsTable = ({ refineCoreProps }: Props) => {
           const product = row.original;
 
           return (
-            <div className="w-full flex flex-col items-center gap-4 md:flex-row md:gap-8 justify-between">
-              <div className="flex flex-row items-center gap-4 md:gap-8">
-                <img
-                  className="h-32 w-32 flex-none rounded-xl object-cover"
-                  src={product.images[0].url}
-                />
-                <div className="flex flex-auto flex-col text-left gap-1">
+            <div className="h-full flex flex-col items-stretch items-center gap-4">
+              <img
+                className="h-32 w-full flex-none rounded-xl object-cover"
+                src={product.images[0].url}
+                alt={product.name}
+              />
+              <div className="h-full flex flex-col items-center justify-between gap-4">
+                <div className="w-full flex flex-col text-left gap-1">
                   <h3 className="text-lg font-bold text-gray-800">
                     {product.name}
                   </h3>
                   <p className="text-gray-600">{product.description}</p>
+                </div>
+                <div className="w-full flex flex-row justify-between">
                   <div className="w-16 text-lg font-bold text-gray-800">
                     {product.price}&nbsp;₽
                   </div>
+                  <div>{isClient && <OrderInput productId={product.id} />}</div>
                 </div>
-              </div>
-              <div className="self-end">
-                {isClient && <OrderInput productId={product.id} />}
               </div>
             </div>
           );
@@ -82,26 +83,24 @@ export const ProductsTable = ({ refineCoreProps }: Props) => {
 
   return (
     <>
-      <table className="w-full">
-        <tbody>
-          {getRowModel().rows.map((row) => {
-            return (
-              <tr key={row.id} className="border-b border-gray-100">
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td key={cell.id} className="p-4">
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 overflow-hidden">
+        {getRowModel().rows.map((row) => {
+          return (
+            <div key={row.id}>
+              {row.getVisibleCells().map((cell) => {
+                return (
+                  <div
+                    key={cell.id}
+                    className="h-full bg-primary/10 rounded-2xl p-4"
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
 
       <div className="py-4 flex justify-center px-4 gap-2">
         <button
